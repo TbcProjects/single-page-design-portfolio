c6f103fc85ee647ad1ffcbff811660dff710f751
TbcProjects <daniel@thebikeclub.co.uk>
2022-09-14 22:05:14 +0100

    add app colors to theme

--- theme/foundations/colors.ts
+++ theme/foundations/colors.ts
@@ -1,3 +1,11 @@
-const colors = {};
+const colors = {
+  light_cream: "#FFF7F0",
+  galactic_blue: "#755CDE",
+  summer_yellow: "#F6A560",
+  pink: "#F39E9E",
+  light_red: "#EB7565",
+  cyan: "#61C4B7",
+  dark_purple: "#552049",
+};
 
 export default colors;
