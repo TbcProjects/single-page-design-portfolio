ce2c1a3f0fa5da6d9baf91c59f25fce03f4dbd86
TbcProjects <daniel@thebikeclub.co.uk>
2022-09-14 22:56:28 +0100

    add app textStyles

--- theme/foundations/textStyles.ts
+++ theme/foundations/textStyles.ts
@@ -1,63 +1,33 @@
 const textStyles = {
   h1: {
-    fontSize: { base: '2.441rem', xl: '3.815rem' },
-    lineHeight: '1.3',
-    fontWeight: 'extrabold',
+    fontSize: { base: "2.25rem", lg: "2.75rem", xl: "3.5rem" },
+    fontWeight: "bold",
+    lineHeight: { base: "2.835rem", lg: "3.465rem", xl: "4.41rem" },
   },
   h2: {
-    fontSize: { base: '1.953rem', xl: '3.052rem' },
-    lineHeight: '1.3',
-    fontWeight: 'extrabold',
+    fontSize: { base: "1.625rem", lg: "2rem", xl: "2.5rem" },
+    fontWeight: "bold",
+    lineHeight: { base: "2.047rem", lg: "2.52rem", xl: "3.15rem" },
   },
   h3: {
-    fontSize: { base: '1.563rem', xl: '2.441rem' },
-    lineHeight: '1.3',
-    fontWeight: 'bold',
+    fontSize: { base: "1.5em", xl: "2rem" },
+    fontWeight: "bold",
+    lineHeight: { base: "1.89rem", xl: "2.52rem" },
   },
-  h4: {
-    fontSize: { base: '1.25rem', xl: '1.953rem' },
-    lineHeight: '1.3',
-    fontWeight: 'bold',
+  body: {
+    fontSize: { base: "1em", xl: "1.125rem" },
+    fontWeight: "bold",
+    lineHeight: { base: "1.625rem", xl: "1.75rem" },
   },
-  h5: {
-    fontSize: { base: '1rem', xl: '1.563rem' },
-    lineHeight: '1.3',
-    fontWeight: 'bold',
+  button: {
+    fontSize: { base: "0.875em", xl: "1rem" },
+    fontWeight: "bold",
+    lineHeight: "1.75rem",
   },
-  h6: {
-    fontSize: { base: '0.8rem', xl: '1.25rem' },
-    lineHeight: '1.3',
-    fontWeight: 'bold',
-  },
-  mainMenu: {
-    fontSize: 'md',
-    fontWeight: 'bold',
-    lineHeight: 'none',
-    textAlign: 'center',
-  },
-  customerMenu: {
-    fontSize: { lg: 'xs', xl: 'sm' },
-    fontWeight: 'normal',
-    lineHeight: 'none',
-  },
-  subMenu: {
-    fontSize: { base: 'md', lg: 'xs', '2xl': 'sm' },
-    fontWeight: 'semibold',
-    lineHeight: 'tall',
-  },
-  mobileMenu: {
-    fontSize: 'md',
-    fontWeight: 'bold',
-    lineHeight: '1',
-  },
-  footerMenu: {
-    fontSize: { base: 'sm', md: 'md' },
-    fontWeight: 'bold',
-    lineHeight: 'base',
-  },
-  smallPrint: {
-    fontSize: '.7rem',
-    lineHeight: 'tall',
+  card: {
+    fontSize: "24rem",
+    fontWeight: "bold",
+    lineHeight: "1.89rem",
   },
 };
 
