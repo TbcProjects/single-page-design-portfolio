2bf8dcc91dab29eb28bd2c28d3679ab25f66c14e
TbcProjects <daniel@thebikeclub.co.uk>
2022-09-16 00:03:45 +0100

    add fontsource plus jakarta sans font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import theme from "../theme";
+import "@fontsource/plus-jakarta-sans"
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
