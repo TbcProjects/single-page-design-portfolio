be2eca7f767f08e81fdeae56586c97716d632ce3
TbcProjects <daniel@thebikeclub.co.uk>
2022-09-18 21:37:05 +0100

    remove boilerplate

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,36 @@
 import type { NextPage } from "next";
 
+import { Container, Text } from "@chakra-ui/react";
+
 const Home: NextPage = () => {
-  return <h1>Single page portfolio</h1>;
+  return (
+    <>
+      <Container>
+        <Text as="h1" textStyle="h1" textAlign={"center"}>
+          Single page portfolio
+        </Text>
+        <Text as="h2" textStyle="h2">
+          Single page portfolio
+        </Text>
+        <Text as="h3" textStyle="h3">
+          Single page portfolio
+        </Text>
+        <Text textStyle="body">Single page portfolio</Text>
+      </Container>
+      <Container bg={"black"}>
+        <Text as="h1" textStyle="h1" textAlign={"center"}>
+          Single page portfolio
+        </Text>
+        <Text as="h2" textStyle="h2">
+          Single page portfolio
+        </Text>
+        <Text as="h3" textStyle="h3">
+          Single page portfolio
+        </Text>
+        <Text textStyle="body">Single page portfolio</Text>
+      </Container>
+    </>
+  );
 };
 
 export default Home;
