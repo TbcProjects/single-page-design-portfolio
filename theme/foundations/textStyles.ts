6e12ed64d75183ead2b5ebed5d2d09a428e32193
TbcProjects <daniel@thebikeclub.co.uk>
2022-09-19 14:26:35 +0100

    add light text styles to textStyles config

--- theme/foundations/textStyles.ts
+++ theme/foundations/textStyles.ts
@@ -4,15 +4,33 @@ const textStyles = {
     fontWeight: "bold",
     lineHeight: { base: "2.835rem", lg: "3.465rem", xl: "4.41rem" },
   },
+  h1_light: {
+    fontSize: { base: "2.25rem", lg: "2.75rem", xl: "3.5rem" },
+    fontWeight: "bold",
+    lineHeight: { base: "2.835rem", lg: "3.465rem", xl: "4.41rem" },
+    color: "light_cream",
+  },
   h2: {
     fontSize: { base: "1.625rem", lg: "2rem", xl: "2.5rem" },
     fontWeight: "bold",
     lineHeight: { base: "2.047rem", lg: "2.52rem", xl: "3.15rem" },
   },
+  h2_light: {
+    fontSize: { base: "1.625rem", lg: "2rem", xl: "2.5rem" },
+    fontWeight: "bold",
+    lineHeight: { base: "2.047rem", lg: "2.52rem", xl: "3.15rem" },
+    color: "light_cream",
+  },
   h3: {
     fontSize: { base: "1.5em", xl: "2rem" },
     fontWeight: "bold",
     lineHeight: { base: "1.89rem", xl: "2.52rem" },
+  },
+  h3_light: {
+    fontSize: { base: "1.5em", xl: "2rem" },
+    fontWeight: "bold",
+    lineHeight: { base: "1.89rem", xl: "2.52rem" },
+    color: "light_cream",
   },
   body: {
     fontSize: { base: "1em", xl: "1.125rem" },
