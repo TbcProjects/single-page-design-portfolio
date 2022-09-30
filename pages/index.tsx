77392e1dfd664236d5854a533cb0410cf466e6bc
TbcProjects <daniel@thebikeclub.co.uk>
2022-09-30 22:34:56 +0100

    remove test elements from index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,33 +1,15 @@
 import type { NextPage } from "next";
 
+import SiteButton from "../components/Elements/Button/SiteButton";
+import BrandLogo from "../components/Elements/Logos/BrandLogo";
+
 import { Container, Text } from "@chakra-ui/react";
 
 const Home: NextPage = () => {
   return (
     <>
       <Container>
-        <Text as="h1" textStyle="h1" textAlign={"center"}>
-          Single page portfolio
-        </Text>
-        <Text as="h2" textStyle="h2">
-          Single page portfolio
-        </Text>
-        <Text as="h3" textStyle="h3">
-          Single page portfolio
-        </Text>
-        <Text textStyle="body">Single page portfolio</Text>
-      </Container>
-      <Container bg={"black"}>
-        <Text as="h1" textStyle="h1" textAlign={"center"}>
-          Single page portfolio
-        </Text>
-        <Text as="h2" textStyle="h2">
-          Single page portfolio
-        </Text>
-        <Text as="h3" textStyle="h3">
-          Single page portfolio
-        </Text>
-        <Text textStyle="body">Single page portfolio</Text>
+        
       </Container>
     </>
   );
