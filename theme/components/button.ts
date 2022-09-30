ac65c51eb04c69b2a36eb8f4ad2af9fb6eda5c84
TbcProjects <daniel@thebikeclub.co.uk>
2022-09-30 22:34:15 +0100

    add sizes for button(md and lg)

--- theme/components/button.ts
+++ theme/components/button.ts
@@ -4,23 +4,31 @@ const Button: ComponentStyleConfig = {
   baseStyle: {
     fontWeight: "bold",
     fontSize: "16px",
-    w: "228px",
-    h: "56px",
+    minH: "56px",
     py: "14px",
     borderRadius: "28px",
   },
-  sizes: {},
+  sizes: {
+    md: {
+      minW: "176px",
+      minH: "44px",
+    },
+    lg: {
+      minW: "228px",
+      minH: "56px",
+    },
+  },
   variants: {
     primary: {
       color: "light_cream",
       bg: "black",
       _hover: {
-        bg: "light_red",
+        bg: "galactic_blue",
       },
     },
     secondary: {
       color: "light_cream",
-      bg: "galactic_blue",
+      bg: "light_red",
       _hover: {
         bg: "summer_yellow",
       },
