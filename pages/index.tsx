478bd77e777d0f8a479b76b17d747bdde521239d
TbcProjects <daniel@thebikeclub.co.uk>
2022-10-01 23:45:40 +0100

    import hero to index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,14 @@
 import type { NextPage } from "next";
 
-import SiteButton from "../components/Elements/Button/SiteButton";
-import BrandLogo from "../components/Elements/Logos/BrandLogo";
+import Hero from "../components/Sections/Hero/Hero";
 
 import { Container, Text } from "@chakra-ui/react";
 
 const Home: NextPage = () => {
   return (
     <>
-      <Container>
-        
+      <Container maxW={'1111px'}>
+        <Hero />
       </Container>
     </>
   );
